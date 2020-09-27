traud/freeswitch

commit eee96c60c99a343332399e45027b4ad03c99b33d

Tweaks

src/mod/applications/mod_spandsp/udptl.c
@@ -205,7 +205,8 @@ int udptl_rx_packet(udptl_state_t *s, const uint8_t buf[], int len)
 	/* Save the new packet. Pure redundancy mode won't use this, but some systems will switch
 	   into FEC mode after sending some redundant packets. */
 	x = seq_no & UDPTL_BUF_MASK;
-	memcpy(s->rx[x].buf, msg, msg_len);
+    if (msg_len > 0)
+    	memcpy(s->rx[x].buf, msg, msg_len);
 	s->rx[x].buf_len = msg_len;
 	s->rx[x].fec_len[0] = 0;
 	s->rx[x].fec_span = 0;
@@ -288,7 +289,8 @@ int udptl_rx_packet(udptl_state_t *s, const uint8_t buf[], int len)
 				return -1;
 
 			/* Save the new FEC data */
-			memcpy(s->rx[x].fec[i], data, s->rx[x].fec_len[i]);
+            if (s->rx[x].fec_len[i])
+    			memcpy(s->rx[x].fec[i], data, s->rx[x].fec_len[i]);
 #if 0
 			switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "FEC: ");
 			for (j = 0; j < s->rx[x].fec_len[i]; j++)

libs/spandsp/src/libspandsp.2012.vcxproj.filters
@@ -696,8 +696,12 @@
     <ClInclude Include="spandsp.h">
       <Filter>Header Files</Filter>
     </ClInclude>
-    <ClInclude Include="spandsp\t4_t6_decode.h" />
-    <ClInclude Include="spandsp\t4_t6_encode.h" />
+    <ClInclude Include="spandsp\t4_t6_decode.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
+    <ClInclude Include="spandsp\t4_t6_encode.h">
+      <Filter>Header Files</Filter>
+    </ClInclude>
     <ClInclude Include="spandsp\ademco_contactid.h">
       <Filter>Header Files</Filter>
     </ClInclude>

commit fc7d32ef024c756322cae692a23a7a81cf59ab69

[mod_spandsp] Fix two white-space errors
Seven years ago, commit eee96c6 introduced two white-space errors. The
compiler Clang 10 error-ed: `misleading indentation; statement is not
part of the previous 'if'`. Now, Clang compiles successfully.

src/mod/applications/mod_spandsp/udptl.c
@@ -207,8 +207,8 @@ int udptl_rx_packet(udptl_state_t *s, const uint8_t buf[], int len)
 	/* Save the new packet. Pure redundancy mode won't use this, but some systems will switch
 	   into FEC mode after sending some redundant packets. */
 	x = seq_no & UDPTL_BUF_MASK;
-    if (msg_len > 0)
-    	memcpy(s->rx[x].buf, msg, msg_len);
+	if (msg_len > 0)
+		memcpy(s->rx[x].buf, msg, msg_len);
 	s->rx[x].buf_len = msg_len;
 	s->rx[x].fec_len[0] = 0;
 	s->rx[x].fec_span = 0;
@@ -296,8 +296,8 @@ int udptl_rx_packet(udptl_state_t *s, const uint8_t buf[], int len)
 				return -1;
 
 			/* Save the new FEC data */
-            if (s->rx[x].fec_len[i])
-    			memcpy(s->rx[x].fec[i], data, s->rx[x].fec_len[i]);
+			if (s->rx[x].fec_len[i])
+				memcpy(s->rx[x].fec[i], data, s->rx[x].fec_len[i]);
 #if 0
 			switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "FEC: ");
 			for (j = 0; j < s->rx[x].fec_len[i]; j++)